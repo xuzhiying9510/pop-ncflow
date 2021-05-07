d2292cf1aa7cffd9f455bc4b301fdc5f2dcc4a18
Deepak Narayanan <deepakn94@gmail.com>
2021-05-06 21:27:26 -0700

    Plotting code

--- plots/lib/cdf_utils.py
+++ plots/lib/cdf_utils.py
@@ -8,8 +8,44 @@ from .plot_utils import (
     print_stats,
     sort_and_set_index,
     filter_by_hyperparams,
+    PF_PARAMS,
     LABEL_NAMES_DICT,
 )
+
+
+def per_iter_to_ncflow_df(per_iter_fname):
+    per_iter_df = filter_by_hyperparams(per_iter_fname).drop(
+        columns=[
+            "num_nodes",
+            "num_edges",
+            "num_commodities",
+            "partition_runtime",
+            "size_of_largest_partition",
+            "iteration",
+            "r1_runtime",
+            "r2_runtime",
+            "recon_runtime",
+            "r3_runtime",
+            "kirchoffs_runtime",
+        ]
+    )
+    ncflow_df = per_iter_df.groupby(
+        [
+            "problem",
+            "tm_model",
+            "traffic_seed",
+            "scale_factor",
+            "total_demand",
+            "algo",
+            "clustering_algo",
+            "num_partitions",
+            "num_paths",
+            "edge_disjoint",
+            "dist_metric",
+        ]
+    ).sum()
+
+    return sort_and_set_index(ncflow_df)
 
 
 def get_ratio_df(other_df, baseline_df, target_col, suffix):
@@ -45,6 +81,59 @@ def get_ratio_df(other_df, baseline_df, target_col, suffix):
         data=results,
     )
     return df
+
+
+def get_ratio_dataframes(
+    path_form_csv,
+    pop_csv,
+    pop_parent_query_str="split_fraction == 0.75 and tm_model == 'poisson-high-intra' or split_fraction == 0.0 and tm_model != 'poisson-high-intra'",
+    pop_query_strs=[],
+    parent_query_str=None,
+    ncflow_csv=None,
+):
+    # Path Formulation DF
+    path_form_df = (
+        pd.read_csv(path_form_csv)
+        .drop(columns=["num_nodes", "num_edges", "num_commodities"])
+        .query(PF_PARAMS)
+    )
+    path_form_df = sort_and_set_index(path_form_df, drop=True)
+    if parent_query_str is not None:
+        path_form_df = path_form_df.query(parent_query_str)
+
+    # POP DF
+    pop_df = pd.read_csv(pop_csv)
+    pop_df = sort_and_set_index(pop_df, drop=True)
+    if parent_query_str is not None:
+        pop_df = pop_df.query(parent_query_str)
+
+    def get_ratio_dfs(parent_df, query_strs, suffix):
+        return {
+            query_str: get_ratio_df(
+                parent_df.query(query_str), path_form_df, "obj_val", suffix
+            )
+            for query_str in query_strs
+        }
+
+    pop_dfs = (
+        get_ratio_dfs(pop_df.query(pop_parent_query_str), pop_query_strs, "_pop")
+        if len(pop_query_strs) > 0
+        else pop_df
+    )
+
+    obj_to_return = {}
+    obj_to_return["POP"] = pop_dfs
+
+    # NCFlow DF
+    if ncflow_csv:
+        ncflow_df = per_iter_to_ncflow_df(ncflow_csv)
+        if parent_query_str is not None:
+            ncflow_df = ncflow_df.query(parent_query_str)
+
+        ncflow_ratio_df = get_ratio_df(ncflow_df, path_form_df, "obj_val", "_ncflow")
+        obj_to_return["NCFlow"] = ncflow_ratio_df
+
+    return obj_to_return
 
 
 def plot_cdfs(
@@ -123,37 +212,3 @@ def plot_cdfs(
         save_figure(fname, extra_artists=extra_artists)
     # plt.show()
 
-
-def per_iter_to_nc_df(per_iter_fname):
-    per_iter_df = filter_by_hyperparams(per_iter_fname).drop(
-        columns=[
-            "num_nodes",
-            "num_edges",
-            "num_commodities",
-            "partition_runtime",
-            "size_of_largest_partition",
-            "iteration",
-            "r1_runtime",
-            "r2_runtime",
-            "recon_runtime",
-            "r3_runtime",
-            "kirchoffs_runtime",
-        ]
-    )
-    nc_iterative_df = per_iter_df.groupby(
-        [
-            "problem",
-            "tm_model",
-            "traffic_seed",
-            "scale_factor",
-            "total_demand",
-            "algo",
-            "clustering_algo",
-            "num_partitions",
-            "num_paths",
-            "edge_disjoint",
-            "dist_metric",
-        ]
-    ).sum()
-
-    return sort_and_set_index(nc_iterative_df)
