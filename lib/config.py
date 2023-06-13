cb66592cf283dd2d83944e65bb70948148a00e65
xuzhiying9510 <xuzhiying9510@hotmail.com>
2023-06-13 00:45:58 -0400

    change to teal directory

--- lib/config.py
+++ lib/config.py
@@ -2,8 +2,10 @@ import os
 
 TL_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 # TOPOLOGIES_DIR = os.path.join(TL_DIR, "topologies")
+# TM_DIR = os.path.join(TL_DIR, "traffic-matrices")
+# use Teal's directory
 TOPOLOGIES_DIR = os.path.join(TL_DIR, "..", "topologies")
-TM_DIR = os.path.join(TL_DIR, "traffic-matrices")
+TM_DIR = os.path.join(TL_DIR, "..", "traffic-matrices")
 TEAVAR_DATA_DIR = os.path.join(TL_DIR, "ext", "teavar", "code", "data")
 TEAVAR_RUNLOGS_DIR = os.path.join(TL_DIR, "ext", "teavar", "code", "runlogs")
 TEAVAR_BASELINE_RESULTS_DIR = os.path.join(
